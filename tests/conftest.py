318e22f22e6dd3b4939aa82a980402798396884e
KateVarg <katye9470@gmail.com>
2024-06-13 20:41:13 +0200

    добавление проверки записей в таблице после отправки формы

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def browser_setting():
     browser.config.base_url = "https://demoqa.com"
-    browser.config.window_height = 1200
+    browser.config.window_height = 1800
     browser.config.window_width = 1200
 
     yield
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,4 +1,4 @@
-from selene import browser
+from selene import browser, have
 import os
 
 
@@ -9,11 +9,11 @@ def test_practice_form():
     browser.element('#lastName').type('Иванов')
     browser.element('#userEmail').type('ivanivanov@test.com')
     browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('89123456789')
+    browser.element('#userNumber').type('8912345678')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select option[value="5"]').click()
     browser.element('.react-datepicker__year-select option[value="1994"]').click()
-    browser.all('.react-datepicker__day--003').second.click()
+    browser.element('.react-datepicker__day.react-datepicker__day--015').click()
     browser.element('#subjectsInput').type('English')
     browser.element('.subjects-auto-complete__menu').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
@@ -25,3 +25,17 @@ def test_practice_form():
     browser.element('#city').click()
     browser.element('#react-select-4-option-1').click()
     browser.element('#submit').click()
+
+    browser.element('.modal-content').element('table').all('tr').all('td').even.should(have.exact_texts(
+            'Иван Иванов',
+            'ivanivanov@test.com',
+            'Male',
+            '8912345678',
+            '15 June,1994',
+            'English',
+            'Reading, Music',
+            'images.jpeg',
+            'Страна, город, улица, дом, этаж, квартира',
+            'NCR Gurgaon',
+        )
+    )
